02af78c0deddeebf98a9e4bc0fc0c08c7b551890
Joseph Papaluca <jpapaluc@gmail.com>
2017-09-17 18:55:50 -0400

    renamed component, component files

---
+++ reactify/src/SpotifyContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class SpotifyContainer extends Component {
+  render() {
+    return (
+      <h1> some html. </h1>
+    );
+  }
+}
+
+export default SpotifyContainer; 
--- reactify/src/index.js
+++ reactify/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SpotifyContainer from './SpotifyContainer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SpotifyContainer />, document.getElementById('root'));
 registerServiceWorker();
